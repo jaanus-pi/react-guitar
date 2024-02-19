2030d9e0b0577fc03525ff0b5f2174802c01a35b
jaanus-pi <jaanus-soft@proton.me>
2024-02-19 15:20:36 +0200

    setup
    Basic fretboard with notes.

---
+++ fretboard-app/src/components/fretboard/Fretboard.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import './Fretboard.css'
+
+const Fretboard = () => {
+  return (
+    <div className="fretboard">
+        <div className="string">
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note single-dot"></div>
+          <div className="fret note"></div>
+          <div className="fret note single-dot"></div>
+          <div className="fret note"></div>
+          <div className="fret note single-dot"></div>
+          <div className="fret note"></div>
+          <div className="fret note single-dot"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note">
+            <div className="double-dot"></div>
+          </div>
+          <div className="fret note"></div>
+        </div>
+        <div className="string">
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+        </div>
+        <div className="string">
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+        </div>
+        <div className="string">
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+        </div>
+        <div className="string">
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+        </div>
+        <div className="string">
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+          <div className="fret note"></div>
+        </div>
+      </div>
+  )
+}
+
+export default Fretboard
